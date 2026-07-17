80defb5b128769f65ee70b7648698b78844567d7
alec016 <aaronquemadadiaz@gmail.com>
2026-07-17 18:04:26 +0200

    fix: crash on core popup when reach certain level of active cores throwing max string packet limit (default)

--- src/main/java/es/degrassi/mmreborn/api/controller/CorePopup.java
+++ src/main/java/es/degrassi/mmreborn/api/controller/CorePopup.java
@@ -1,16 +1,24 @@
 package es.degrassi.mmreborn.api.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.JsonOps;
 import es.degrassi.mmreborn.api.codec.NamedCodec;
+import io.netty.handler.codec.DecoderException;
+import io.netty.handler.codec.EncoderException;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.util.GsonHelper;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Map;
 
 public record CorePopup(int maxCores, Map<String, List<CompoundTag>> pages) {
+  private static final Gson GSON = new Gson();
   private static final Codec<Map<String, List<CompoundTag>>> PAGES = NamedCodec.unboundedMap(
       NamedCodec.STRING,
       NamedCodec.of(CompoundTag.CODEC).listOf(),
@@ -21,14 +29,17 @@ public record CorePopup(int maxCores, Map<String, List<CompoundTag>> pages) {
     @Override
     public @NotNull CorePopup decode(RegistryFriendlyByteBuf buffer) {
       int maxCores = buffer.readInt();
-      Map<String, List<CompoundTag>> pages = buffer.readJsonWithCodec(PAGES);
+      JsonElement jsonelement = GsonHelper.fromJson(GSON, buffer.readUtf(Integer.MAX_VALUE), JsonElement.class);
+      DataResult<Map<String, List<CompoundTag>>> dataresult = PAGES.parse(JsonOps.INSTANCE, jsonelement);
+      Map<String, List<CompoundTag>> pages = dataresult.getOrThrow(s -> new DecoderException("Failed to decode json: " + s));
       return new CorePopup(maxCores, pages);
     }
 
     @Override
     public void encode(RegistryFriendlyByteBuf buffer, CorePopup data) {
       buffer.writeInt(data.maxCores);
-      buffer.writeJsonWithCodec(PAGES, data.pages);
+      DataResult<JsonElement> dataresult = PAGES.encodeStart(JsonOps.INSTANCE, data.pages);
+      buffer.writeUtf(GSON.toJson(dataresult.getOrThrow(s -> new EncoderException("Failed to encode: " + s + " " + data.pages))), Integer.MAX_VALUE);
     }
   };
 }
